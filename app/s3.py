988f099d8e1b99e46f8f694394e794323d17ca13
Oliver Plunkett <otplunkett@gmail.com>
2019-01-03 00:19:26 -0500

    test

--- app/s3.py
+++ app/s3.py
@@ -1,21 +1,21 @@
 import boto3
 import os
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', "")
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', "")
-print AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+# AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', "")
+# AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', "")
+# print AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
-if os.getenv('IS_HEROKU') != True:
-    try:
-        import config
-        AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
-        AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
-    except ImportError:
-      pass
+# if os.getenv('IS_HEROKU') != True:
+#     try:
+#         import config
+#         AWS_ACCESS_KEY_ID = config.AWS_ACCESS_KEY_ID
+#         AWS_SECRET_ACCESS_KEY = config.AWS_SECRET_ACCESS_KEY
+#     except ImportError:
+#       pass
 
 session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_access_key_id="AKIAJMA2CQOJ42TJJBFA",
+    aws_secret_access_key="WcbdU3Rhuvl02xzR95pETD1+4UaDrzHji8rfGjLFWcbdU3Rhuvl02xzR95pETD1+4UaDrzHji8rfGjLF",
     region_name="us-east-1"
 )
 
