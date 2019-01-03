ee03d0799d07a4f8956260866fd4018fc5566dee
Oliver Plunkett <otplunkett@gmail.com>
2019-01-03 00:13:01 -0500

    c

--- app/s3.py
+++ app/s3.py
@@ -3,6 +3,7 @@ import os
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', "")
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', "")
+print AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 
 if os.getenv('IS_HEROKU') != True:
     try:
@@ -14,7 +15,8 @@ if os.getenv('IS_HEROKU') != True:
 
 session = boto3.Session(
     aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name="us-east-1"
 )
 
 s3_client = boto3.client('s3')
