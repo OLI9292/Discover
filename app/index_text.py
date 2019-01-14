95613f755e06774f056e322de8be10afdfce3926
Oliver Plunkett <otplunkett@gmail.com>
2019-01-14 18:23:25 -0500

    change buildpack

--- app/index_text.py
+++ app/index_text.py
@@ -37,7 +37,6 @@ from es import es_client
 def filename_to_title(filename):
     return filename.replace(".pdf", "").replace(".epub", "").replace(".txt", "").replace("_", " ").title()
 
-
 def ocr_pdf(pdf):
     if os.path.exists("tmp") == False:
         os.makedirs("tmp")
