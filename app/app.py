f37a4167e1f3550a44ff98af4d89253f130bcf4e
Oliver Plunkett <otplunkett@gmail.com>
2019-08-25 20:22:39 -0400

    nada

--- app/app.py
+++ app/app.py
@@ -131,3 +131,4 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     print("running...")
     app.run(host='0.0.0.0', debug=False, port=port)
+    # app.run(host='0.0.0.0', debug=False, port=5003)
